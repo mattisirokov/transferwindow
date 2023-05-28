8f2d67b6db184b42073d365adba56efe41f6eff1
mattisirokov <mattisirokov@gmail.com>
2023-05-28 13:56:11 +0300

    Basic set up with firebase

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,30 @@
-import "@/styles/global.css";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 
+const theme = extendTheme({
+  colors: {
+    brand: {
+      green: "#9beb34",
+    },
+  },
+  fonts: {
+    body: "Inter, sans-serif",
+    heading: "Inter, sans-serif",
+  },
+  styles: {
+    global: {
+      body: {
+        bg: "black",
+        color: "white",
+      },
+    },
+  },
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
+import { Button } from "@chakra-ui/react";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
-      <h1 className="text-white">Home</h1>
+      <h1>Home</h1>
+      <Link href="/login">
+        <Button bg="brand.green" color="black">
+          Login
+        </Button>
+      </Link>
     </div>
   );
 }
